b2f36057ed147d4700780470361cb207048006cf
Sunny4123 <151112974+Sunny4123@users.noreply.github.com>
2024-10-09 16:59:04 +0700

    5

--- dist/Test1.js
+++ dist/Test1.js
@@ -2,34 +2,31 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Utils_1 = require("./Utils");
 let allTestsPassed = true;
-if (Utils_1.Utils.calculate(1, 1, '+') === 2) {
+if (Utils_1.Utils.calculate(1, 1, '+') === 4) {
     console.log(0);
 }
 else {
     console.log(1001);
     allTestsPassed = false;
 }
-if (Utils_1.Utils.calculate(5, 3, '-') === 2) {
-    console.log(0);
-}
-else {
-    console.log(1002);
-    allTestsPassed = false;
-}
-if (Utils_1.Utils.calculate(3, 4, '*') === 10) {
-    console.log(0);
-}
-else {
-    console.log(1003);
-    allTestsPassed = false;
-}
-if (Utils_1.Utils.calculate(10, 2, '/') === 5) {
-    console.log(0);
-}
-else {
-    console.log(1004);
-    allTestsPassed = false;
-}
-if (!allTestsPassed) {
-    console.log(1005);
-}
+// if (Utils.calculate(5, 3, '-') === 2) {
+//     console.log(0);
+// } else {
+//     console.log(1002);
+//     allTestsPassed = false;
+// }
+// if (Utils.calculate(3, 4, '*') === 10) {
+//     console.log(0);
+// } else {
+//     console.log(1003);
+//     allTestsPassed = false;
+// }
+// if (Utils.calculate(10, 2, '/') === 5) {
+//     console.log(0);
+// } else {
+//     console.log(1004);
+//     allTestsPassed = false;
+// }
+// if (!allTestsPassed) {
+//     console.log(1005);
+// }
--- src/Test1.ts
+++ src/Test1.ts
@@ -4,34 +4,34 @@ import { Utils } from "./Utils";
 
 let allTestsPassed = true;
 
-if (Utils.calculate(1, 1, '+') === 2) {
+if (Utils.calculate(1, 1, '+') === 4) {
     console.log(0);
 } else {
     console.log(1001);
     allTestsPassed = false;
 }
 
-if (Utils.calculate(5, 3, '-') === 2) {
-    console.log(0);
-} else {
-    console.log(1002);
-    allTestsPassed = false;
-}
+// if (Utils.calculate(5, 3, '-') === 2) {
+//     console.log(0);
+// } else {
+//     console.log(1002);
+//     allTestsPassed = false;
+// }
 
-if (Utils.calculate(3, 4, '*') === 10) {
-    console.log(0);
-} else {
-    console.log(1003);
-    allTestsPassed = false;
-}
+// if (Utils.calculate(3, 4, '*') === 10) {
+//     console.log(0);
+// } else {
+//     console.log(1003);
+//     allTestsPassed = false;
+// }
 
-if (Utils.calculate(10, 2, '/') === 5) {
-    console.log(0);
-} else {
-    console.log(1004);
-    allTestsPassed = false;
-}
+// if (Utils.calculate(10, 2, '/') === 5) {
+//     console.log(0);
+// } else {
+//     console.log(1004);
+//     allTestsPassed = false;
+// }
 
-if (!allTestsPassed) {
-    console.log(1005);
-}
+// if (!allTestsPassed) {
+//     console.log(1005);
+// }
